c1d445bf530b1e5a8768b28feb68254308b40bdd
Masum Billah <masumbillah6584@gmail.com>
2022-11-14 23:24:25 +0600

    added contact us section

---
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <div className="w-96 text-center">
+          <h5 className="text-primary font-bold mb-2">Contact Us</h5>
+          <h3 className="text-3xl mb-6">Stay Connected with us</h3>
+          <div>
+            <form>
+              <input
+                className="input input-bordered w-full mb-5"
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+              <input
+                className="input input-bordered w-full mb-5"
+                type="text"
+                name="sub"
+                placeholder="Subject"
+              />
+              <textarea
+                className="w-full block textarea textarea-bordered mb-5"
+                placeholder="Your Message..."
+                rows="4"
+                cols=""
+              />
+              <input className="btn btn-primary" type="submit" value="Submit" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Appointment from "./Appointment/Appointment";
 import Banner from "./Banner/Banner";
+import ContactUs from "./ContactUs/ContactUs";
 import DentalCare from "./DentalCare/DentalCare";
 import InfoCard from "./InfoCard/InfoCard";
 import Services from "./Services/Services";
@@ -15,6 +16,7 @@ const Home = () => {
       <DentalCare />
       <Appointment />
       <Testimonial />
+      <ContactUs />
     </div>
   );
 };
