2cfa53e060f9aaf5a34091e7e55c7a9607e84d00
Masum Billah <masumbillah6584@gmail.com>
2022-11-15 12:34:32 +0600

    fix responsive on login and signup page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,50 +3,48 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="hero">
-      <div className="hero-content">
-        <div className="card shadow-2xl">
-          <div className="card-body w-96">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                className="input input-bordered"
-              />
-              <label className="label">
-                <Link to="/" className="label-text-alt link link-hover">
-                  Forgot password?
-                </Link>
-              </label>
-            </div>
-            <div className="form-control mt-1">
-              <button className="btn btn-secondary">Login</button>
-            </div>
-            <p className="label-text-alt">
-              New to doctor's Home ?
-              <Link className="text-primary text-sm" to="/signup">
-                create an account
+    <div className="flex justify-center items-center md:my-32">
+      <div className="card shadow-2xl">
+        <div className="card-body max-w-sm">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="password"
+              className="input input-bordered"
+            />
+            <label className="label">
+              <Link to="/" className="label-text-alt link link-hover">
+                Forgot password?
               </Link>
-            </p>
-            <h4 className="divider m-0">OR</h4>
-            <div className="form-control">
-              <button className="btn btn-outline btn-primary">
-                Continue With Google
-              </button>
-            </div>
+            </label>
+          </div>
+          <div className="form-control mt-1">
+            <button className="btn btn-secondary">Login</button>
+          </div>
+          <p className="label-text-alt">
+            New to doctor's Home ?
+            <Link className="text-primary text-sm" to="/signup">
+              create an account
+            </Link>
+          </p>
+          <h4 className="divider m-0">OR</h4>
+          <div className="form-control">
+            <button className="btn btn-outline btn-primary">
+              Continue With Google
+            </button>
           </div>
         </div>
       </div>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -3,60 +3,58 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div className="hero">
-      <div className="hero-content">
-        <div className="card shadow-2xl">
-          <div className="card-body w-96">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Name"
-                className="input input-bordered w-full"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                className="input input-bordered"
-              />
-              <label className="label">
-                <Link to="/" className="label-text-alt link link-hover">
-                  Forgot password?
-                </Link>
-              </label>
-            </div>
-            <div className="form-control mt-1">
-              <button className="btn btn-secondary">Sign UP</button>
-            </div>
-            <p className="label-text-alt">
-              Already Have an account?
-              <Link className="text-primary" to="/login">
-                Login Here!
+    <div className="flex justify-center items-center my-20">
+      <div className="card shadow-2xl">
+        <div className="card-body max-w-sm w-full">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="password"
+              className="input input-bordered"
+            />
+            <label className="label">
+              <Link to="/" className="label-text-alt link link-hover">
+                Forgot password?
               </Link>
-            </p>
-            <h4 className="divider m-0">OR</h4>
-            <div className="form-control">
-              <button className="btn btn-outline btn-primary">
-                Continue With Google
-              </button>
-            </div>
+            </label>
+          </div>
+          <div className="form-control mt-1">
+            <button className="btn btn-secondary">Sign UP</button>
+          </div>
+          <p className="label-text-alt">
+            Already Have an account?
+            <Link className="text-primary" to="/login">
+              Login Here!
+            </Link>
+          </p>
+          <h4 className="divider m-0">OR</h4>
+          <div className="form-control">
+            <button className="btn btn-outline btn-primary">
+              Continue With Google
+            </button>
           </div>
         </div>
       </div>
