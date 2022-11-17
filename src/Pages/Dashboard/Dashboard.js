ac40a70d9ca2f4786a9968b610fb845a8fa13123
Masum Billah <masumbillah6584@gmail.com>
2022-11-17 18:37:46 +0600

    show users booking

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>dashboard is comming soon</div>;
+};
+
+export default Dashboard;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../Layout/DashboardLayout";
 import Main from "../Layout/Main";
 import Appointment from "../Pages/Appointment/Appointment";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
+import MyAppointments from "../Pages/MyAppointments/MyAppointments";
 import SignUp from "../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
@@ -15,5 +17,10 @@ export const router = createBrowserRouter([
       { path: "/login", element: <Login /> },
       { path: "/signup", element: <SignUp /> },
     ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [{ path: "/dashboard", element: <MyAppointments /> }],
   },
 ]);
